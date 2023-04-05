9f7bba7defd9e335bf27af95ff6f8968b9bc812e
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-04-05 21:02:41 +0530

    electron package updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function createWindow() {
 
   // Create the main window with the screen size
   mainWindow = new BrowserWindow({ width, height, show: false });
-  mainWindow.loadURL('https://test.infobus.in/login');
+  mainWindow.loadURL('https://infobus.in/login');
 
   // Show the loading screen while the page is loading
   mainWindow.once('ready-to-show', () => {
