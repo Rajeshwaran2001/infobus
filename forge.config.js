a7e7df2705c8b9983094c639ac48f7adc5e3d2a0
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-04-05 21:43:12 +0530

    GitHub publisher added

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,21 @@
+const { GithubPublisher } = require('@electron-forge/publisher-github');
+
 module.exports = {
   packagerConfig: {},
   rebuildConfig: {},
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'Rajeshwaran2001',
+          name: 'infobus',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
