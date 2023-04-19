7bb11afd7998930849672c09db0fadb13eb20467
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-04-19 19:04:56 +0530

    customer view

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function createWindow() {
 
   // Create the main window with the screen size
   mainWindow = new BrowserWindow({ width, height, show: false });
-  mainWindow.loadURL('https://infobus.in/login');
+  mainWindow.loadURL('https://infobus.in/customer-view/');
 
   // Show the loading screen while the page is loading
   mainWindow.once('ready-to-show', () => {
